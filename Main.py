8cd5bf70b684014a7735d822f9009cde1803f6e6
URK21CS2045   SASANK <96973169+URK21CS2045@users.noreply.github.com>
2022-11-07 21:08:56 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,31 +1,20 @@
 from typing import List
 
-def swap(arr,j,k):
-  temp=arr[j]
-  arr[j]=arr[k]
-  arr[k]=temp
-
 def selectionSort(array, size) -> List[int]:
   # Write your code here
-  if size==1:
-    return array
-  for i in range(0,size-1):
-    low=i
-    execution=0
+  for i in range(size):
+    min=i
     for j in range(i+1,size):
-      if array[low]>array[j]:
-        low=j
-        execution=1
-    if execution!=0:
-      swap(array,i,low)
-  return array
-  
+      if array[j] < array[min]:
+        min=j
+    (array[i],array[min]) = (array[min],array[i])
 # Do not change the following code
 input_data = input()
 data = []
 for item in input_data.split(', '):
   if item.isnumeric():
     data.append(int(item))
-  elif item.lstrip("-").isnumeric():		           
+  elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(selectionSort(data, len(data)))
+selectionSort(data, len(data))
+print(data)
